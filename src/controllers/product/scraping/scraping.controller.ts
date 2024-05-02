940aa8cb08fdcaf3291b2b698f006c1797cf8190
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-02 01:38:00 -0300

    Added migrations sequelize Closes #17

--- src/controllers/product/scraping/scraping.controller.ts
+++ src/controllers/product/scraping/scraping.controller.ts
@@ -10,12 +10,13 @@ import { ProductFlagM } from "../../../database/models/product/product-flag/prod
 import { ProductColorM } from "../../../database/models/product/product-color/product-color.model";
 import { ProductSizeM } from "../../../database/models/product/product-size/product-size.model";
 import { ProductDetailM } from "../../../database/models/product/product-detail/product-detail.model";
-import CREDENTIALS from "../../../../urban-vogue-410216-e4add7f801f7.json"
 
-// Configuration for the client
 const translate = new Translate({
-  credentials: CREDENTIALS,
-  projectId: CREDENTIALS.project_id,
+  projectId: "urban-vogue-421615",
+  credentials: {
+    client_email: "esdras@urban-vogue-421615.iam.gserviceaccount.com",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCWgaoZY4+X2CGq\nM1erzL0FiU7F5zlLwkdDxrg31Te0fM/8xLs6G/m+7W8nX+2dlRGoDZOEIjPpLCys\nVRev/pNy2tlxeovwaOSCTjjFILit9hlk7RXyYj1sHwXQHcp86kUSjVsRk7fCDKmH\nOQCsoN2ZfCZXSUKU3c+17zULqsyLQy50S/1JihSUv4dZm8OvaNSWZuZ5d3K7J7ND\nhKzqB39J3sCatTcbhOATEkwnDayy0/I5lI/DAFvBVuTu/p90aFfZcnLu7zR7G3T3\nAErwrEh7/cvrXokRGg+i5gp1TrKF/gP9L4Y24HPj7naOgqMLcb60pI2Vi2ZOGKQS\nqgoJcPvFAgMBAAECggEABMHM601ZCsgvSjaiDlaj4SgASCOL4qUy75q5PwnbdKDe\n9GLlywtQJxTQnFU9HgEJ7ulHm20hqGdNHdZOWnGeCKA6WrVFn9/k3GnWBARfKfUb\nGN711bR9XgHorbW0rSLnc35pyDdbycObhnVgBtTKeZvcXLUT/b9kCGCq6SHpIfv9\nbl21EkUILFAVjzdTqmHG8Ro2NNz/BO2D86iGR23A2v0c286OKtWY4icuWRUDi9Ui\nKDZO20DzCuU55dA3hkc+AKMAfIxdY6BM+a4SXIFii7rmnSqFGn9lEuZ7WnzRVS6Z\nxH9ra5wJeVdPY/iAzThqRxBnloAp4/20+Y3SoPqLqQKBgQDQny3M24BCo4j1/M0O\npgGM8GqAd54j1VWOnOBA/kN5tbaOJG3Azvnxg5Acn2VnbH9P56HPniSBfsqQdMuw\nGLeSsv5j42Bx1L1aTeBPUFmOTQPs2rglvE4amgfL3S1/zrTwaNVg3Apy4KIIBD/P\nLHp8XcxguFYduUg04YxSkph83QKBgQC4r8mxwY9fhRrGzB8v6wn+WGfmyStlDL8Y\nQq8ZVvvRKyc80obdieGXRFl0Dqbijt09dhyt4v2I2x/LdKzFQb7YTPCZOs6MmjEA\nwuMR7zk7LGXK1GfHTPL7MBRawVhphDitMji5zLoATpmX8Bl5iD4q2ZjzuiaEnXlM\nqyb70nF4CQKBgQCkrhKEVHzhnZ/etcjUmFw++gTIFiSCGsPnyJQJGez/pV5OxBjT\nC08nw3e98GEmi/HPjIQhlK8yfXi/IUMsZcZ7Ccpf5Tvbf1hdcX3wfffDqa3AiUK1\nnvh0bURcO+LPsccIw2g0hl3FIucktt371xt1EsHjU3+/BOQnwid3gvrWDQKBgCQY\nr3ofk68eq8oZEStja9ObYctYnDOm1x6dwe1Y8+TA421e1F8uWLog8L7fMjzroVAm\n6poN2HHMKh2w0yfGns9V0lyQgtg2NseoT4ssz2nCVq7a7uGkJ4Vysze9vFL7Nbyc\nKTwfSGdQ9XIdykzXlRb46o0QYADhaljSxs7HJchJAoGBAJWIx5rN3M21kemCD7kZ\ne2Gls2KKJJGChD4TA6ou2/bc5mXINKiw6U9Za8kg6CCyPDII1bo2Sp8cLNlJ7XtL\n7OE5pPzMPK7g6ggZekPbWFBhCTZsE319EREG41Hq/1tzZgtEZYKT8ihw6xrT7HjV\nrLrm0dmlppQ+oSJIsGgucIkG\n-----END PRIVATE KEY-----\n",
+  },
 });
 const tokenizer = new natural.WordTokenizer();
 const stopWords = new Set([
