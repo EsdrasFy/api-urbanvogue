5e3a97f55587d30983c10c32d1f29aa036f6584a
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-06 13:16:48 -0300

    Sistema para alterar dados sensíveis quase finalizado

--- src/utils/sms/twilio/sms-twilio.utils.ts
+++ src/utils/sms/twilio/sms-twilio.utils.ts
@@ -1,16 +1,17 @@
 import twilio from "twilio";
-import "dotenv/config"
+import "dotenv/config";
 
 const TwilioSid = process.env.TWILIOID;
-const AuthTwilio = process.env.TWILIO_AUTH;
+const TwilioAuth = process.env.TWILIO_AUTH;
+const TwilioPhone = process.env.TWILIO_PHONE;
 
-const client = twilio(TwilioSid, AuthTwilio);
+const client = twilio(TwilioSid, TwilioAuth);
 
 async function SmsTwilio(): Promise<void> {
   try {
     const message = await client.messages.create({
       body: "This is code for verification",
-      from: "+12176353724",
+      from: TwilioPhone,
       to: "+5511949163426",
     });
 
