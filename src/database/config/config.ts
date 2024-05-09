dc0d0d1379c798826c0f5ac83ee3300a39b344db
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-09 00:40:46 -0300

    fe

--- src/database/config/config.ts
+++ src/database/config/config.ts
@@ -2,11 +2,11 @@ require("dotenv").config();
 import { Options } from "sequelize";
 
 const config: Options = {
-  username: process.env.DB_USER || undefined,
-  password: process.env.DB_PASSWORD || undefined,
-  database: process.env.DB_NAME || undefined,
-  host: process.env.DB_HOST || undefined,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+  username: "root" || undefined,
+  password: "OIfLBnLXGcpPVLjktObWlsvBllgqGZAP" || undefined,
+  database: "railway" || undefined,
+  host: "viaduct.proxy.rlwy.net" || undefined,
+  port: 37488,
   dialect: "mysql",
   dialectModule: require("mysql2"),
   logging: true,
