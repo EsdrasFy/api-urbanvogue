2d934be6f28873f2e1a35a7e8a6ce684f256337d
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-09 14:45:57 -0300

    Fixed cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://urbanvogue.cloud", "https://github.com", "https://accounts.google.com"],
+  origin: ["http://localhost:3000", "https://urbanvogue.cloud", "https://github.com", "https://accounts.google.com", "https://api.urbanvogue.cloud"],
   methods: ["GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT"],
   allowedHeaders: ["X-CSRF-Token", "X-Requested-With", "Accept", "Accept-Version", "Content-Length", "Content-MD5", "Content-Type", "Date", "X-Api-Version"],
   credentials: true
