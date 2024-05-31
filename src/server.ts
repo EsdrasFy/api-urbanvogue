c2fc8aa081bbe381a3015b94d1c8fa934b889145
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-31 18:17:53 -0300

    fix: cors in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import dotenv from "dotenv";
 import app from "./app";
 // import "./service/whatsapp-web/whatsapp-web.service";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
 app.use(cookieParser());
 const PORT = process.env.PORT;
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://github.com",
+  "https://urbanvogue.cloud",
+];
 
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 try {
   app.listen(PORT, () => {
     console.log(`Servidor rodando em ${PORT}`);
