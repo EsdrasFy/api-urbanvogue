c414c7e3031614d1c62e2100949c113bef782c40
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-31 18:13:39 -0300

    fix: env variable

--- src/controllers/oauth/oauth-github/oauth-github.controller.ts
+++ src/controllers/oauth/oauth-github/oauth-github.controller.ts
@@ -1,16 +1,18 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response } from 'express';
-import { GetGithubOAuthTokens, GetGithubUser } from '../../../service/oauth/oauth-github/oauth-github.service';
-import { GithubUserResult } from '../../../service/user/types';
-
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import {
+  GetGithubOAuthTokens,
+  GetGithubUser,
+} from "../../../service/oauth/oauth-github/oauth-github.service";
+import { GithubUserResult } from "../../../service/user/types";
 
 const OAuthGithub = async (req: Request, res: Response) => {
-  const frontBaseUrl = process.env.FRONT_BASE_URL as string;
+  const success = process.env.OAUTH_SUCCESS_URL as string;
   const secret = process.env.SECRET as string;
 
   const { code } = req.query;
   if (!code) {
-    throw new Error('Dont have code');
+    throw new Error("Dont have code");
   }
 
   try {
@@ -25,17 +27,17 @@ const OAuthGithub = async (req: Request, res: Response) => {
     });
 
     const token = jwt.sign({ name, avatar, githubId }, secret, {
-      expiresIn: '24h',
+      expiresIn: "24h",
     });
 
     return res
-      .cookie('jwt', token, {
+      .cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 24 * 60 * 60 * 1000, // 24h
       })
-      .redirect(frontBaseUrl || 'http://localhost:3000');
+      .redirect(success || "http://localhost:3000");
   } catch (error: any) {
     throw new Error(error.message);
   }
